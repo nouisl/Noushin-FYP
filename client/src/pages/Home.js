c4d304ccdb1a147075580397d1a18eecc211666b
nouisl <78652710+nouisl@users.noreply.github.com>
2023-02-09 00:50:50 +0000

    fixes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import bg from "../images/bg.png"
 import { ConnectButton, Select, DatePicker, Input } from "web3uikit";
 import { FaSearch } from 'react-icons/fa';
 
@@ -56,18 +57,19 @@ function SearchBar() {
 function Home() {
   return (
     <>
-      <div className="container">
-        <Header />
-        <SearchBar />
+      <Header />
+      <SearchBar />
+ 
         <div className="column">
           <p></p>
           <h1>THIS IS HOME</h1>
-          <div className="row align-items-center">
-            <div className="d-flex">
-              <p className="mx-auto">
-                <Link to="/db"><button className="btn btn-light">start shopping now</button></Link>
-              </p>
-            </div>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="d-flex">
+                <p className="mx-auto">
+                  <Link to="/db"><button className="btn btn-light">start shopping now</button></Link>
+                </p>
+              </div>
           </div>
         </div>
       </div>
